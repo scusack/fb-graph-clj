0efd2f34369c4e4c82238c16194e46fef13eed2c
Simon Cusack <simon.cusack@gmail.com>
2012-02-22 13:39:55 +1100

    Upgrades clj-http.

--- project.clj
+++ project.clj
@@ -2,6 +2,6 @@
   :description "Simplified access to the Facebook Graph API."
   :dependencies [[org.clojure/clojure "1.3.0"]
                  ;;
-                 [clj-http "0.2.5"]
-                 [cheshire "2.0.2"]]
+                 [clj-http "0.3.2"]
+                 [cheshire "2.1.0"]]
   :dev-dependencies [[swank-clojure "1.4.0-SNAPSHOT"]])
