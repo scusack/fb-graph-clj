59e240f41d695327b77838b94015c33bb5e1151b
Simon Cusack <simon.cusack@gmail.com>
2012-02-22 14:07:03 +1100

    Adds URL to project.

--- project.clj
+++ project.clj
@@ -1,5 +1,6 @@
 (defproject fb-graph-clj "1.0.0-SNAPSHOT"
-  :description "Simplified access to the Facebook Graph API."
+  :description  "Simplified access to the Facebook Graph API."
+  :url          "https://github.com/scusack/fb-graph-clj"
   :dependencies [[org.clojure/clojure "1.3.0"]
                  ;;
                  [clj-http "0.3.2"]
