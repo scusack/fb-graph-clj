0ede159566b23065b93afb016df03b06de7e08a5
Simon Cusack <simon.cusack@gmail.com>
2012-05-14 14:38:17 +1000

    Upgrades dependencies.

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject fb-graph-clj "1.0.0-SNAPSHOT"
   :description  "Simplified access to the Facebook Graph API."
   :url          "https://github.com/scusack/fb-graph-clj"
-  :dependencies [[org.clojure/clojure "1.3.0"]
+  :dependencies [[org.clojure/clojure "1.4.0"]
                  ;;
-                 [clj-http "0.3.2"]
-                 [cheshire "2.1.0"]]
+                 [clj-http "0.4.1"]
+                 [cheshire "4.0.0"]]
   :dev-dependencies [[swank-clojure "1.4.0-SNAPSHOT"]])
